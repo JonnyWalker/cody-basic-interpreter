fe4c003c915cb6287406a286e639266c8b886192
iTitus <iTitus@users.noreply.github.com>
2025-02-10 22:10:03 +0100

    introduce IO object for abstraction

--- cody_interpreter.py
+++ cody_interpreter.py
@@ -1,13 +1,26 @@
 from cody_parser import ASTTypes
+from abc import ABC, abstractmethod
+from typing import Optional, Iterable
+
+
+class IO(ABC):
+    @abstractmethod
+    def print(self, value): ...
+
+    @abstractmethod
+    def println(self): ...
+
+    @abstractmethod
+    def input(self) -> str: ...
 
 
 class Interpreter:
-    def __init__(self):
-        self.cody_output_log = []  # used for test cases and maybe (later) for debugging
+    def __init__(self, io: Optional[IO] = None):
         self.int_arrays = {}  # maps variable names to values
         self.string_arrays = {}  # maps variable names to values
         self.call_stack = []
         self.loop_stack = []
+        self.io = io if io is not None else StdIO()
 
     def compute_target(self, node):
         if node.ast_type == ASTTypes.ArrayExpression:
@@ -98,6 +111,48 @@ class Interpreter:
             index, target = self.compute_target(command.lvalue)
             value = self.eval(command.rvalue)
             self.add_value(target, value, index)
+        elif command.command_type == "PRINT":
+            value = ""
+            for expr in command.expressions:
+                # TODO: implement AT and TAB functions in eval via IO
+                v = self.eval(expr)
+                if v is not None:
+                    self.io.print(str(v))
+
+            if not command.no_new_line:
+                self.io.println()
+        elif command.command_type == "INPUT":
+            for expr in command.expressions:
+                index, target = self.compute_target(expr)
+                value = self.io.input()
+                self.add_value(target, value, index)
+        elif command.command_type == "IF":
+            value = self.eval(command.condition)
+            if value:
+                self.run_command(command.command)
+        elif command.command_type == "GOTO":
+            number = self.eval(command.expression)
+            assert isinstance(number, int)
+            # TODO: precompute hashmap with jump target to do this in O(1)
+            for index, target in enumerate(self.code):
+                if target.line_number == number:
+                    self.next_index = index
+                    break
+        elif command.command_type == "FOR":
+            self.run_command(command.assignment)
+            limit = self.eval(command.limit)
+            variable = command.assignment.lvalue
+            assert variable.ast_type == ASTTypes.IntegerVariable
+            self.loop_stack.append((variable.name, limit, self.next_index))
+            assert self.int_arrays[variable.name][0] < limit
+        elif command.command_type == "NEXT":
+            name, limit, next_index = self.loop_stack[-1]
+            current_value = self.int_arrays[name][0]
+            if current_value == limit:
+                pass
+            else:
+                self.int_arrays[name][0] = current_value + 1
+                self.next_index = next_index
         elif command.command_type == "GOSUB":
             number = self.eval(command.expression)
             assert isinstance(number, int)
@@ -107,56 +162,6 @@ class Interpreter:
                 if target.line_number == number:
                     self.next_index = index
                     break
-        elif command.command_type == "PRINT":
-            value = ""
-            for expr in command.expressions:
-                # TODO: implement AT and TAB functions
-                value += str(self.eval(expr))
-
-            if not self.cody_output_log or self.last_print_had_new_line:
-                self.cody_output_log.append(value)
-            else:
-                self.cody_output_log[-1] += value
-
-            if command.no_new_line:
-                # https://stackoverflow.com/questions/493386/how-to-print-without-a-newline-or-space
-                print(value, end="")
-                self.last_print_had_new_line = False
-            else:
-                print(value)
-                self.last_print_had_new_line = True
-        elif command.command_type == "IF":
-            value = self.eval(command.condition)
-            if value:
-                self.run_command(command.command)
-        elif command.command_type == "INPUT":
-            for expr in command.expressions:
-                index, target = self.compute_target(expr)
-                value = input("? ")
-                self.add_value(target, value, index)
-        elif command.command_type == "GOTO":
-            number = self.eval(command.expression)
-            assert isinstance(number, int)
-            # TODO: precompute hashmap with jump target to do this in O(1)
-            for index, target in enumerate(self.code):
-                if target.line_number == number:
-                    self.next_index = index
-                    break
-        elif command.command_type == "NEXT":
-            name, limit, next_index = self.loop_stack[-1]
-            current_value = self.int_arrays[name][0]
-            if current_value == limit:
-                pass
-            else:
-                self.int_arrays[name][0] = current_value + 1
-                self.next_index = next_index
-        elif command.command_type == "FOR":
-            self.run_command(command.assignment)
-            limit = self.eval(command.limit)
-            variable = command.assignment.lvalue
-            assert variable.ast_type == ASTTypes.IntegerVariable
-            self.loop_stack.append((variable.name, limit, self.next_index))
-            assert self.int_arrays[variable.name][0] < limit
         elif command.command_type == "RETURN":
             self.next_index = self.call_stack.pop()
         elif command.command_type == "END":
@@ -171,3 +176,35 @@ class Interpreter:
             command = code[self.next_index]
             self.next_index += 1
             self.run_command(command)
+
+
+class StdIO(IO):
+    def print(self, value: str):
+        print(value, end="")
+
+    def println(self):
+        print()
+
+    def input(self) -> str:
+        return input("? ")
+
+
+class TestIO(IO):
+    def __init__(self, inputs: Optional[Iterable[str]] = None):
+        self.inputs = list(inputs) if inputs else None
+        self.output_log: list[str] = []
+        # flag to indicate that the last input ended on a newline
+        self.new_line: bool = True
+
+    def print(self, value: str):
+        if self.new_line:
+            self.output_log.append("")
+            self.new_line = False
+        self.output_log[-1] += value
+
+    def println(self):
+        self.print("")
+        self.new_line = True
+
+    def input(self) -> str:
+        return str(self.inputs.pop(0))
--- tests/test_interpreter.py
+++ tests/test_interpreter.py
@@ -1,124 +1,102 @@
 # python -m pytest -s
 from cody_parser import CodyBasicParser
-from cody_interpreter import Interpreter
+from cody_interpreter import Interpreter, TestIO
+
+
+def run_code(code: str) -> Interpreter:
+    parser = CodyBasicParser()
+    parsed = parser.parse_string(code)
+    interp = Interpreter(TestIO())
+    interp.run_code(parsed)
+    return interp
 
 
 def test_simple_add():
     code = "10 PRINT 3+4"  # book page 247
-    parser = CodyBasicParser()
-    command = parser.parse_line(code)
-    interp = Interpreter()
-    interp.run_command(command)
-    assert interp.cody_output_log == ["7"]
+    interp = run_code(code)
+    assert interp.io.output_log == ["7"]
 
 
 def test_hello_world():
     code = '10 PRINT "HELLO"'
-    parser = CodyBasicParser()
-    command = parser.parse_line(code)
-    interp = Interpreter()
-    interp.run_command(command)
-    assert interp.cody_output_log == ["HELLO"]
+    interp = run_code(code)
+    assert interp.io.output_log == ["HELLO"]
 
 
 def test_no_new_line_print():
     code = '10 PRINT "WHAT IS YOUR NAME";'  # book page 250
-    parser = CodyBasicParser()
-    command = parser.parse_line(code)
-    interp = Interpreter()
-    interp.run_command(command)
-    assert interp.cody_output_log == ["WHAT IS YOUR NAME"]
+    interp = run_code(code)
+    assert interp.io.output_log == ["WHAT IS YOUR NAME"]
 
 
 def test_expression_list():
     code = '50 PRINT "CODY"," IS ",14," YEARS OLD."'  # book page 250 (modified)
-    parser = CodyBasicParser()
-    command = parser.parse_line(code)
-    interp = Interpreter()
-    interp.run_command(command)
-    assert interp.cody_output_log == ["CODY IS 14 YEARS OLD."]
+    interp = run_code(code)
+    assert interp.io.output_log == ["CODY IS 14 YEARS OLD."]
 
 
 def test_array_expression():
     code = "10 A(0)=10"  # book page 253
-    parser = CodyBasicParser()
-    command = parser.parse_line(code)
-    interp = Interpreter()
-    interp.run_command(command)
+    interp = run_code(code)
     assert interp.int_arrays["A"][0] == 10
 
 
 def test_variable_example():
-    code = [
-        "10 A(0)=10",
-        "20 A(1)=20",
-        "30 PRINT A+A(1)*3",
-    ]  # book page 253
-    parser = CodyBasicParser()
-    parsed_code = parser.parse_program(code)
-    interp = Interpreter()
-    interp.run_code(parsed_code)
+    code = """
+10 A(0)=10
+20 A(1)=20
+30 PRINT A+A(1)*3
+"""  # book page 253
+    interp = run_code(code)
     assert interp.int_arrays["A"][0] == 10
     assert interp.int_arrays["A"][1] == 20
-    assert interp.cody_output_log == ["70"]
+    assert interp.io.output_log == ["70"]
 
 
 def test_variable_example2():
-    code = [
-        '10 M$ = "HELLO "',
-        '20 N$ = "WORLD!"',
-        "30 PRINT M$,N$",
-    ]  # book page 254
-    parser = CodyBasicParser()
-    parsed_code = parser.parse_program(code)
-    interp = Interpreter()
-    interp.run_code(parsed_code)
+    code = """
+10 M$ = "HELLO "
+20 N$ = "WORLD!"
+30 PRINT M$,N$
+"""  # book page 254
+    interp = run_code(code)
     assert interp.string_arrays["M"][0] == "HELLO "
     assert interp.string_arrays["N"][0] == "WORLD!"
-    assert interp.cody_output_log == ["HELLO WORLD!"]
+    assert interp.io.output_log == ["HELLO WORLD!"]
 
 
 def test_goto_example():
-    code = [
-        '10 PRINT "A"',
-        "20 GOTO 40",
-        '30 PRINT "B"',
-        '40 PRINT "Z"',
-    ]  # book page 257
-    parser = CodyBasicParser()
-    parsed_code = parser.parse_program(code)
-    interp = Interpreter()
-    interp.run_code(parsed_code)
-    assert interp.cody_output_log == ["A", "Z"]
+    code = """
+10 PRINT "A"
+20 GOTO 40
+30 PRINT "B"
+40 PRINT "Z"
+"""  # book page 257
+    interp = run_code(code)
+    assert interp.io.output_log == ["A", "Z"]
 
 
 def test_gosub_example():
-    code = [
-        '10 PRINT "A"',
-        "20 GOSUB 50",
-        '30 PRINT "C"',
-        "40 END",
-        '50 PRINT "B"',
-        "60 RETURN",
-    ]  # book page 257
-    parser = CodyBasicParser()
-    parsed_code = parser.parse_program(code)
-    interp = Interpreter()
-    interp.run_code(parsed_code)
-    assert interp.cody_output_log == ["A", "B", "C"]
+    code = """
+10 PRINT "A"
+20 GOSUB 50
+30 PRINT "C"
+40 END
+50 PRINT "B"
+60 RETURN
+"""  # book page 257
+    interp = run_code(code)
+    assert interp.io.output_log == ["A", "B", "C"]
 
 
 def test_for_example():
-    code = [
-        "10 FOR I=1 TO 5",
-        "20 PRINT I",
-        "30 NEXT",
-    ]  # book page 259
-    parser = CodyBasicParser()
-    parsed_code = parser.parse_program(code)
-    interp = Interpreter()
-    interp.run_code(parsed_code)
-    assert interp.cody_output_log == ["1", "2", "3", "4", "5"]
+    code = """
+10 FOR I=1 TO 5
+20 PRINT I
+30 NEXT
+"""  # book page 259
+    interp = run_code(code)
+    assert interp.io.output_log == ["1", "2", "3", "4", "5"]
 
 
 def test_rel_ops():
@@ -142,11 +120,8 @@ def test_rel_ops():
 340 IF 2>1 THEN PRINT 34
 350 IF 2>=1 THEN PRINT 35
 """
-    parser = CodyBasicParser()
-    parsed_code = parser.parse_string(code)
-    interp = Interpreter()
-    interp.run_code(parsed_code)
-    assert interp.cody_output_log == [
+    interp = run_code(code)
+    assert interp.io.output_log == [
         "10",
         "13",
         "15",
@@ -171,53 +146,47 @@ def test_print():
 80 PRINT "B";
 90 PRINT
 """
-    parser = CodyBasicParser()
-    parsed_code = parser.parse_string(code)
-    interp = Interpreter()
-    interp.run_code(parsed_code)
-    assert interp.cody_output_log == ["1", "23", "45678", "", "AB"]
+    interp = run_code(code)
+    assert interp.io.output_log == ["1", "23", "45678", "", "AB"]
 
 
 def test_math_expr():
     code = """
 10 PRINT 4+5*6-10
-"""
-    parser = CodyBasicParser()
-    parsed_code = parser.parse_string(code)
-    interp = Interpreter()
-    interp.run_code(parsed_code)
-    assert interp.cody_output_log == ["24"]
+"""  # book page 270
+    interp = run_code(code)
+    assert interp.io.output_log == ["24"]
+
+
+def test_math_div():
+    code = """
+10 PRINT 16/5
+"""  # book page 270
+    interp = run_code(code)
+    assert interp.io.output_log == ["3"]
 
 
 def test_math_expr_parens():
     code = """
 10 PRINT 3*((8+2)/2)
-"""
-    parser = CodyBasicParser()
-    parsed_code = parser.parse_string(code)
-    interp = Interpreter()
-    interp.run_code(parsed_code)
-    assert interp.cody_output_log == ["15"]
+"""  # book page 271
+    interp = run_code(code)
+    assert interp.io.output_log == ["15"]
 
 
 def test_math_expr_unary_minus_1():
     code = """
-10 A=1
-20 PRINT -A
-"""
-    parser = CodyBasicParser()
-    parsed_code = parser.parse_string(code)
-    interp = Interpreter()
-    interp.run_code(parsed_code)
-    assert interp.cody_output_log == ["-1"]
+10 PRINT -(1+2)
+"""  # book page 271
+    interp = run_code(code)
+    assert interp.io.output_log == ["-3"]
 
 
 def test_math_expr_unary_minus_2():
     code = """
-10 PRINT -(1+2)
-"""
-    parser = CodyBasicParser()
-    parsed_code = parser.parse_string(code)
-    interp = Interpreter()
-    interp.run_code(parsed_code)
-    assert interp.cody_output_log == ["-3"]
+10 A=20
+20 B=2
+30 PRINT -A*B
+"""  # book page 272
+    interp = run_code(code)
+    assert interp.io.output_log == ["-40"]
